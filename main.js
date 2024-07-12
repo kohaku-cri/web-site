c92d2321ffeb81fc3121ac01cb8251eaf6660637
こはく <kohaku.second.aka@gmail.com>
2024-07-12 18:11:19 +0900

    メッセージ修正

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // ここからメッセージ見ないでもろて・・byこはく
 
 function showAlert() {
-    alert('ワァ！見つかったァ\n次回の舞台は神社！');
+    alert("ワァ！見つかったァ\n次回の舞台は神社！そして過去最高規模！\nでも制作が難航しまくってるらしいよ");
 }
-document.addEventListener('DOMContentLoaded', (event) => {
-    const specificLink = document.getElementById('specific-link');
-    specificLink.addEventListener('click', showAlert);
+document.addEventListener("DOMContentLoaded", (event) => {
+    const specificLink = document.getElementById("specific-link");
+    specificLink.addEventListener("click", showAlert);
 });
